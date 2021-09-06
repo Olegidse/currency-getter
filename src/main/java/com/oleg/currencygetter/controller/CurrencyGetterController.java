ce978c792593d6301231eca6fd33f2d7865eafa5
Олег Романов <romanovolegd@gmail.com>
2021-09-06 14:16:50 +0300

    add swagger annotation

--- src/main/java/com/oleg/currencygetter/controller/CurrencyGetterController.java
+++ src/main/java/com/oleg/currencygetter/controller/CurrencyGetterController.java
@@ -2,16 +2,12 @@ package com.oleg.currencygetter.controller;
 
 import com.oleg.currencygetter.client.GiphyApiInterface;
 import com.oleg.currencygetter.service.GiphyService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.constraints.Pattern;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -23,19 +19,12 @@ public class CurrencyGetterController {
     private final GiphyApiInterface giphyApiInterface;
     private final GiphyService giphyService;
 
+    @Operation(summary = "Метод для получения gif изображения по коду валюты",
+            description = "Код валюты - 3 латинские буквы")
     @GetMapping(value = "/get-gif/{currencyCode}", produces = MediaType.IMAGE_GIF_VALUE)
     public ResponseEntity<Object> downloadGif(@PathVariable String currencyCode) throws URISyntaxException {
         URI gifURI = new URI(giphyService.getGifUri(currencyCode));
         byte[] gifFile = giphyApiInterface.getGif(gifURI);
-
-//        File outputFile = new File("C:\\Users\\dima\\IdeaProjects\\currency-getter\\src\\main\\resources", "giffInBytesTest");
-//        try (FileOutputStream outputStream = new FileOutputStream(outputFile)) {
-//            outputStream.write(gifFile);
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
         return ResponseEntity
                 .ok()
                 .contentLength(gifFile.length)
